cae2dac3597cf0666f51b96da5c96aba3cca2440
Samuel Adeyemo <crownprincesamuelaadeyemo@gmail.com>
2022-11-18 17:29:56 +0100

    Worked on some parts on the task management app, more like trying to transition it from just a todoapp to a full functioning task management software.

--- mytodoapp/views.py
+++ mytodoapp/views.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'todoapp.html')
+
+def calendar(request):
+    return render(request, 'calendar.html')
+
+def createTask(request):
+    if request.method == "POST":
+        pass
